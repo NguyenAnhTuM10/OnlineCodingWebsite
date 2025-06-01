af3c990df59dda93baf937d971634741387d1576
NguyenAnhTuM10 <151209084+NguyenAnhTuM10@users.noreply.github.com>
2025-06-02 03:52:31 +0700

    start enrollment

--- backend/routes/enrollmentsRoute.js
+++ backend/routes/enrollmentsRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {authenticate} = require('../middlewares/authMiddleware');
 const { enrollCourse,getMyEnrollments } = require('../controllers/enrollmentsController');
 
-
+//app.use('/api/enrollments', enrollmentRoutes);
 // dùng để đăng ký khóa học
 // Bảo vệ bằng JWT
 router.post('/', authenticate, enrollCourse); // ddawng ký khóa học
